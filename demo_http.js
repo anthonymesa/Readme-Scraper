2149db3ab6858e719232a01b73006e4377a22c52
Anthony Mesa <admesa@pm.me>
2021-05-22 13:04:03 -0400

    working

--- demo_http.js
+++ demo_http.js
@@ -1,3 +1,4 @@
+
 const http = require("http");
 const fs = require('fs').promises;
 const { execSync } = require('child_process');
@@ -7,51 +8,44 @@ const { parse } = require('node-html-parser');
 const host = '0.0.0.0';
 const port = 8083;
 
-
-function parseOutput(input)
-{
-	console.log('parsing');
-	const root = parse(input);
-	const page = root.querySelector('article');
-	//fs.writeFileSync(__dirname + "/index.html", page.toString() + "</body></html>", err => {
-	//	if(err) {
-	//		console.log(err);
-	//		return;
-	//	}
-	//});
-	//console.log(page.toString());
+function getArticle(cb){
+    
+    console.log("getting article");
+    
+    var command = 'curl -s https://github.com/anthonymesa/resume/blob/main/README.md';
+    
+    console.log("running exec");
+    
+	var stdout = execSync(command);
 	
-console.log('parsed ' + page.toString());
-
-	return page;
+    const root = parse(stdout);
+    const page = root.querySelector('article');
+		
+	cb(page);
 }
 
-const requestListener = function(req, res) {
+const requestListener = function(req, res) 
+{
 	fs.readFile(__dirname + "/index.html")
 	.then(contents => {
+	    
 		res.setHeader("Content-Type", "text/html");
 		res.writeHead(200);
-	console.log('starting');	
+		
+	    console.log('starting');
+	    
 		var index_root = parse(contents);
-console.log(		index_root.toString());
-		var body = index_root.querySelector('body');
+		
+        console.log("index root parsed");
+        
+		var body = index_root.querySelector('#resume');
 
-		var resume = execSync('curl https://github.com/anthonymesa/resume/blob/main/README.md', (error, stdout, stderr) => {
-				if(error) {
-							console.error('exec error: ${error}');
-							return;
-						}
-			console.log('appending child');
-							console.log('appended child');
-							console.error('stderr: ${stderr}');
-				const article = parseOutput(stdout);
-			return article;
-		});
+        console.log("body parsed");
 
-		body.appendChild(resume);
-
-
-		res.end(index_root.toString());
+        getArticle((article) => {
+	    	body.appendChild(article);
+	     	res.end(index_root.toString());
+        });
 	})
 	.catch(err => {
 		res.writeHead(500);
@@ -61,6 +55,7 @@ console.log(		index_root.toString());
 };
 
 const server = http.createServer(requestListener);
+
 server.listen(port, host, () => {
 	console.log('server is running');
 });
